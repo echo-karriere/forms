5dd34015f05df7b3737514355ea1ff92d799314e
helges1 <sebhelg@gmail.com>
2023-01-16 02:59:56 +0100

    config change

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = {
+  env: {
+    AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
+    AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID,
+  },
+};
