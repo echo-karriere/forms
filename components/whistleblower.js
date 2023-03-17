6e21d4c5c3d63e91ff0c374fca97108db8e4d954
helges1 <sebhelg@gmail.com>
2023-03-17 20:03:31 +0100

    Update whistleblower.js

--- components/whistleblower.js
+++ components/whistleblower.js
@@ -1,10 +1,15 @@
 import Airtable from "airtable";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import "remixicon/fonts/remixicon.css";
 
 export default function Home() {
   const formRef = useRef();
   const [success, setSuccess] = useState(false);
+  const [referenceCode, setReferenceCode] = useState("");
+
+  useEffect(() => {
+    setReferenceCode(generateRandomString(10));
+  }, []);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -14,11 +19,10 @@ export default function Home() {
     );
 
     const Varsel = document.getElementById("Varsel").value;
-    const Referansekode = document.getElementById("Referansekode").value;
 
     base("varsel").create(
       {
-        Referansekode: Referansekode,
+        Referansekode: referenceCode,
         Varsel: Varsel,
       },
       function (err, record) {
@@ -32,16 +36,6 @@ export default function Home() {
     );
   };
 
-  function generateRandomString(length) {
-    let result = "";
-    let characters = "abcdefghijklmnopqrstuvwxyz";
-    for (let i = 0; i < length; i++) {
-      result += characters.charAt(
-        Math.floor(Math.random() * characters.length)
-      );
-    }
-    return result.concat("-", Math.floor(Math.random() * 89 + 11));
-  }
   return (
     <div>
       <>
@@ -76,75 +70,86 @@ export default function Home() {
             vare på referansekoden, for eksempel ved å ta et skjembilde av den,
             eller ved å skrive den ned på et papir eller i notatene dine. Etter
             du har sendt inn varslet ditt vil du ikke få tilgang til
-            referansekoden igjen. Referansekoden din består av 10 små bokstaver,
-            en bindestrek, og 2 tall.
           </p>
           <br />
           <input
             type="text"
             className="border py-2 px-4 mb-5 mt-2 max-w-2xl focus:outline-none focus:border-b-[#66023C] focus:bg-gray-200 focus:border-b-2"
             id="Referansekode"
-            value={generateRandomString(10)}
+            value={referenceCode}
             disabled
           />
-
           <br />
-          <h2 className="text-2xl font-semibold mt-3">Del 2 - varsling</h2>
         </div>
 
-        <form
-          className="w-11/12 max-w-xl mx-auto mt-4"
-          ref={formRef}
-          onSubmit={submitHandler}>
-          <div className="flex flex-col max-w-2xl text-sm mb-2">
-            <label htmlFor="Varsel" className="font-bold">
-              Skriv din varslingsmelding her:
-            </label>
-            <small>
-              Om du oppgir kontaktinformasjon vil vi kontakte deg via de
-              kanalene du oppgir. Dersom du ikke oppgir kontaktinformasjon vil
-              vi kontakte deg via ixb.no/whistleblower.
-            </small>
+        <div className="mx-auto w-11/12 max-w-xl my-4">
+          <h3 className="text-2xl font-semibold mt-3">Del 2 - Varsel</h3>
+          <br />
+          <p>
+            Vennligst beskriv kort hva som har skjedd. Du kan gjerne bruke
+            punktlister eller avsnitt for å gjøre det lettere å lese. Vi vil
+            behandle denne informasjonen konfidensielt og kun benytte den til å
+            gjøre våre arrangementer tryggere.
+          </p>
+          <br />
+          <form ref={formRef} onSubmit={submitHandler}>
             <textarea
-              className="border py-2 px-4 mb-5 mt-2 max-w-2xl focus:outline-none focus:border-b-[#66023C] focus:bg-gray-200 focus:border-b-2"
               id="Varsel"
-              type="text"
-              rows="5"
-              required
-            />
-          </div>
-          <br /> <br />
-          <button
-            type="submit"
-            className="bg-[#33030d] hover:bg-fuchsia-900 text-white py-2 px-4"
-            // Show an success message on form submition
-            onClick={() => {
-              if (Varsel.value !== "") setSuccess(true);
-            }}>
-            Send varsel
-          </button>
-          {success && (
-            // add a success message over the entire page
-            <div className="fixed top-0 left-0 w-full h-full bg-white bg-opacity-90 flex items-center justify-center">
-              <div className="bg-white p-8 rounded shadow-2xl">
-                <h2 className="text-2xl font-semibold mb-4">
-                  Varslet ditt er sendt!
-                </h2>
-                <p>
-                  Vi vil nå behandle varslet ditt, og ta kontakt med deg hvis
-                  det er nødvendig. Du kan nå forlate siden.
-                </p>
-                <br />
-                <a
-                  className="bg-[#33030d] hover:bg-fuchsia-900 text-white py-2 px-4"
-                  href="https://ixb.no/whistleblower">
-                  Gå til ixb.no/whistleblower
-                </a>
+              className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
+              rows="4"
+              placeholder="Skriv ditt varsel her"
+              required></textarea>
+            <br />
+            <button
+              type="submit"
+              onClick={() => {
+                if (Varsel.value !== "") setSuccess(true);
+              }}
+              className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+              Send inn varsel
+            </button>
+            {success && (
+              // add a success message over the entire page
+              <div className="fixed top-0 left-0 w-full h-full bg-black flex items-center justify-center">
+                <div className="bg-white p-8 rounded shadow-2xl">
+                  <h2 className="text-2xl font-semibold mb-4">
+                    Varslet ditt er sendt!
+                  </h2>
+                  <p>
+                    Vi vil nå behandle varslet ditt, og ta kontakt med deg hvis
+                    det er nødvendig. Du kan nå forlate siden.
+                  </p>
+                  <br />
+                  <p>Din referansekode er:</p>
+                  <p className="font-bold">
+                    <span className="text-green-600">{referenceCode}</span>
+                  </p>
+                  <small>
+                    Dette er siste gang du ser referansekoden din. Husk å ta
+                    vare på den.
+                  </small>
+                  <br />
+                  <br />
+                  <a
+                    className="bg-[#33030d] hover:bg-fuchsia-900 text-white py-2 px-4"
+                    href="https://ixb.no/whistleblower">
+                    Gå til ixb.no/whistleblower
+                  </a>
+                </div>
               </div>
-            </div>
-          )}
-        </form>
+            )}
+          </form>
+        </div>
       </>
     </div>
   );
+}
+
+function generateRandomString(length) {
+  let result = "";
+  let characters = "abcdefghijklmnopqrstuvwxyz";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result.concat("-", Math.floor(Math.random() * 89 + 11));
 }
